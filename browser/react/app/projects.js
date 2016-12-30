8746477fd8931c6690c7ce63062ebec6507d4237
dye784 <damon.ye91@gmail.com>
2016-12-30 13:14:31 -0500

    added tabs to projects page router bug needs to be fixed

--- browser/react/app/projects.js
+++ browser/react/app/projects.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Tabs, Tab } from 'react-materialize';
 
 const Projects = () => {
   return (
-    <div>
-      <h1>PROJECTS</h1>
-    </div>
+    <Tabs className='tab-demo z-depth-1'>
+        <Tab title="Test 1">Test 1</Tab>
+        <Tab title="Test 2" active>Test 2</Tab>
+        <Tab title="Test 3">Test 3</Tab>
+        <Tab title="Test 4">Test 4</Tab>
+    </Tabs>
   )
 }
 
