9c6e28820b0a13b5b919444aa5c1670df713e1a9
dye784 <damon.ye91@gmail.com>
2017-03-06 18:06:25 -0500

    changed requests https

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 const webpack = require('webpack');
 
 module.exports = {
-  entry: ['./js/bootstrap.min.js', './js/custom.js', './js/jquery.flexslider.js', './js/jquery.js', './js/smoothscroll.js', './js/wow.min.js'/*'./js/imagesloaded.min.js', */ ],
+  entry: ['./js/bootstrap.min.js', './js/custom.js', './js/jquery.flexslider.js', './js/jquery.js', './js/smoothscroll.js', './js/wow.min.js', './js/imagesloaded.min.js'],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
